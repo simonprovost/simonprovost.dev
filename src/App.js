0f8fee15682da9319e8e018c1f315a497e769932
Provost Simon <simon1.provost@epitech.eu>
2024-12-07 01:23:33 +0000

    chore: add Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import researchConfig from "./configs/researchConfig";
+import connectConfig from "./configs/connectConfig";
+import aboutConfig from "./configs/aboutConfig";
+import openSourceConfig from "./configs/openSourceConfig";
+import OpenSource from "./screens/openSource/openSource";
+import About from "./screens/about/about";
+import Research from "./screens/research/research";
+import Connect from "./screens/connect/connect";
+import Credits from "./screens/credits/credits";
+import creditsConfig from "./configs/creditsConfig";
+import Blog from "./screens/blog/blog";
+import BlogPost from "./components/blogPost/blogPost";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<OpenSource {...openSourceConfig} />}/>
+                <Route path="/about" element={<About {...aboutConfig} />}/>
+                <Route path="/research" element={<Research {...researchConfig} />}/>
+                <Route path="/connect" element={<Connect {...connectConfig} />}/>
+                <Route path="/credits" element={<Credits {...creditsConfig} />}/>
+                <Route path="/blog/" element={<Blog />}/>
+                <Route path="/blog/:id/:title" element={<BlogPost />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
