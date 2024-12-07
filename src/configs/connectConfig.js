b00b0b4180b673b3036f6ba8a4d1adf86d02d3b4
Provost Simon <simon1.provost@epitech.eu>
2024-12-07 01:51:28 +0000

    refactor(connectConfig): remove ResearchGate

--- src/configs/connectConfig.js
+++ src/configs/connectConfig.js
@@ -4,7 +4,6 @@ const connectConfig = {
         {title: "GitHub", url: "https://github.com/simonprovost"},
         {title: "LinkedIn", url: "https://www.linkedin.com/in/simonprovostdev/"},
         {title: "Google Scholar", url: "https://scholar.google.com/citations?user=Lv_LddYAAAAJ&hl=en&oi=ao"},
-        {title: "ResearchGate", url: "https://www.researchgate.net/profile/Provost-Simon"},
         {title: "ORCID", url: "https://orcid.org/0000-0001-8402-5464"},
         {title: "Stack Overflow", url: "https://stackoverflow.com/users/9814037/simon-provost"},
         {title: "Email", url: "mailto:s.g.provost@kent.ac.uk"},
