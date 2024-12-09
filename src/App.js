1d6a0e8782c1159bf22867eb101f28138071a446
Provost Simon <simon1.provost@epitech.eu>
2024-12-09 17:00:12 +0000

    chore: add vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import researchConfig from "./configs/researchConfig";
 import connectConfig from "./configs/connectConfig";
 import aboutConfig from "./configs/aboutConfig";
 import openSourceConfig from "./configs/openSourceConfig";
+import creditsConfig from "./configs/creditsConfig";
 import OpenSource from "./screens/openSource/openSource";
 import About from "./screens/about/about";
 import Research from "./screens/research/research";
 import Connect from "./screens/connect/connect";
 import Credits from "./screens/credits/credits";
-import creditsConfig from "./configs/creditsConfig";
 import Blog from "./screens/blog/blog";
 import BlogPost from "./components/blogPost/blogPost";
 import { injectSpeedInsights } from "@vercel/speed-insights";
@@ -30,6 +31,7 @@ const App = () => {
                 <Route path="/blog/" element={<Blog />} />
                 <Route path="/blog/:id/:title" element={<BlogPost />} />
             </Routes>
+            <Analytics />
         </Router>
     );
 };
