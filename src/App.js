284792d03c2bdf3602615d1cd8bc4d255b4d2dcb
Provost Simon <simon1.provost@epitech.eu>
2025-06-04 11:38:32 +0100

    refactor(components): remove custom notion blog in favour of substack

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
 import researchConfig from "./configs/researchConfig";
 import connectConfig from "./configs/connectConfig";
@@ -11,9 +11,15 @@ import About from "./screens/about/about";
 import Research from "./screens/research/research";
 import Connect from "./screens/connect/connect";
 import Credits from "./screens/credits/credits";
-import Blog from "./screens/blog/blog";
-import BlogPost from "./components/blogPost/blogPost";
 import { injectSpeedInsights } from "@vercel/speed-insights";
+
+function RedirectToSubstack() {
+  useEffect(() => {
+    window.location.replace("https://substack.com/@simonprovost");
+  }, []);
+
+  return null;
+}
 
 const App = () => {
     useEffect(() => {
@@ -28,8 +34,7 @@ const App = () => {
                 <Route path="/research" element={<Research {...researchConfig} />} />
                 <Route path="/connect" element={<Connect {...connectConfig} />} />
                 <Route path="/credits" element={<Credits {...creditsConfig} />} />
-                <Route path="/blog/" element={<Blog />} />
-                <Route path="/blog/:id/:title" element={<BlogPost />} />
+                <Route path="/blog/" element={<RedirectToSubstack />} />
             </Routes>
             <Analytics />
         </Router>
