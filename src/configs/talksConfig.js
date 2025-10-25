1cef471e2cb1effeeb4e0d8f8474091a2172f7c0
Provost Simon <simon1.provost@epitech.eu>
2025-10-25 11:52:07 +0100

    refactor(talk): add BIBM talk

--- src/configs/talksConfig.js
+++ src/configs/talksConfig.js
@@ -23,14 +23,36 @@ const talksConfig = {
     ],
     posts: [
         {
+            id: "sepwav-stacking-ai-2025",
+            titles: ["SepWav w/ Stacking for Longitudinal Classification"],
+            details: ["AI-2025 (BCS SGAI), Cambridge, UK", "Dec. 2025"],
+            media: {
+                type: "iframe",
+                src: "https://embed.figma.com/slides/kwRQKCUvF3bhJ5IZEU1CrY/Slides-BCS-SGAI?node-id=1-295&embed-host=share",
+                aspect: "landscape",
+                title: "AI-2025 Slides",
+            },
+        },
+        {
             id: "automated-ml-aida-2025",
             titles: ["Automated Machine Learning For Longitudinal Classification"],
-            details: ["University of Kent @ AIDA Lab", "Nov. 2025"],
+            details: ["AIDA (UKC), Canterbury, UK", "Nov. 2025"],
             media: {
                 type: "iframe",
                 src: "https://embed.figma.com/slides/87E2IvG37lkmcxHxuUsSHW/Slides-AIDA?node-id=1-288&embed-host=share",
                 aspect: "landscape",
                 title: "AIDA Slides",
+            },
+        },
+        {
+            id: "auto-sklong-bibm-2024",
+            titles: ["Auto-Sklong: A New AutoML System for Longitudinal Classification"],
+            details: ["BIBM", "Lisbon, Portugal", "Dec. 2024"],
+            media: {
+                type: "iframe",
+                src: "https://embed.figma.com/slides/hXJUNQENx8LV7jucKDZlR8/BIBM-2024?node-id=1-588&embed-host=share",
+                aspect: "landscape",
+                title: "BIBM Slides",
             },
         },
     ],
