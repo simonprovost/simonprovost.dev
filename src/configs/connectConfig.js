2d8a00275ac076711592f5031b553e2995163f73
Provost Simon <simon1.provost@epitech.eu>
2025-11-29 23:58:01 +0000

    refactor(screens): add external OSS contribs to connect

--- src/configs/connectConfig.js
+++ src/configs/connectConfig.js
@@ -1,13 +1,17 @@
 const connectConfig = {
     showTitle: "Connect",
     contactMethods: [
-        {title: "GitHub", url: "https://github.com/simonprovost"},
+        {title: "GitHub (profile)", url: "https://github.com/simonprovost"},
+        {
+            title: "Open-Source External Contribs (Pull Requests)",
+            url: "https://github.com/search?q=is%3Apr+author%3Asimonprovost+-repo%3Asimonprovost%2Fdeep_forest_py310+-repo%3Asimonprovost%2Fscikit-longitudinal+-repo%3Asimonprovost%2FAuto-Sklong+-repo%3AVIDA-NYU%2FUrbanMapper",
+        },
         {title: "LinkedIn", url: "https://www.linkedin.com/in/simonprovostdev/"},
         {title: "Google Scholar", url: "https://scholar.google.com/citations?user=Lv_LddYAAAAJ&hl=en&oi=ao"},
         {title: "ORCID", url: "https://orcid.org/0000-0001-8402-5464"},
+        {title: "Email", url: "mailto:simon.gilbert.provost@gmail.com"},
         {title: "Blog", url: "https://simonprovost.substack.com/"},
         {title: "Stack Overflow", url: "https://stackoverflow.com/users/9814037/simon-provost"},
-        {title: "Email", url: "mailto:simon.gilbert.provost@gmail.com"},
         {title: "Bento", url: "https://bento.me/simon-provost"},
     ],
 };
