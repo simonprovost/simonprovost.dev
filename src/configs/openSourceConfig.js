80df89940c549ad60f8fe912693aac77844aeaf1
Provost Simon <simon1.provost@epitech.eu>
2025-11-29 23:58:12 +0000

    refactor(screens): simplify homepage OSS

--- src/configs/openSourceConfig.js
+++ src/configs/openSourceConfig.js
@@ -23,7 +23,7 @@ const openSourceConfig = {
     posts: [
         {
             titles: ["Scikit-Longitudinal"],
-            details: ["70+ stars | Sklearn-like Longitudinal ML Library"],
+            details: ["★ 70+ | Sklearn-Like Longitudinal Health-ML Library"],
             media: {
                 type: "video",
                 src: "videos/sklong.webm",
@@ -33,7 +33,7 @@ const openSourceConfig = {
         },
         {
             titles: ["Auto-Sklong"],
-            details: ["40+ stars | AutoML system for Longitudinal ML"],
+            details: ["★ 40+ | AutoML System For Health-Longitudinal ML"],
             media: {
                 type: "video",
                 src: "videos/auto_sklong.webm",
@@ -43,7 +43,7 @@ const openSourceConfig = {
         },
         {
             titles: ["Urban Mapper"],
-            details: ["60+ stars | Sklearn-like Spatio-Urban Analysis"],
+            details: ["★ 60+ | Sklearn-like Spatio-Urban Analysis Pipeline"],
             media: {
                 type: "video",
                 src: "videos/urban_mapper.webm",
@@ -52,54 +52,14 @@ const openSourceConfig = {
             url: "https://github.com/VIDA-NYU/UrbanMapper",
         },
         {
-            titles: ["(M)odel-(C)ontext-(P)rotocols Orchestrator"],
-            details: ["10+ stars | Stack your fav. MCPs"],
+            titles: ["MCP Stack"],
+            details: ["★ 10+ | (M)odel-(C)ontext-(P)rotocols Orchestrator"],
             media: {
                 type: "video",
                 src: "videos/mcpstack.webm",
                 aspect: "landscape",
             },
             url: "https://github.com/MCP-Pipeline/MCPStack",
-        },
-        {
-            titles: ["AutoDDG: From POC to Modern Python Library"],
-            details: ["Accepted | Open-Source @ NYU (VIDA)"],
-            media: {
-                type: "video",
-                src: "videos/github_3.tiny.webm",
-                aspect: "landscape",
-            },
-            url: "https://github.com/VIDA-NYU/AutoDDG/pull/4",
-        },
-        {
-            titles: ["AutoDDG: Multi-LLM Providers & Generators"],
-            details: ["In-Review | Open-Source @ NYU (VIDA)"],
-            media: {
-                type: "video",
-                src: "videos/github_4.tiny.webm",
-                aspect: "landscape",
-            },
-            url: "https://github.com/VIDA-NYU/AutoDDG/pull/6",
-        },
-        {
-            titles: ["GAMA x SMAC3"],
-            details: ["In-Review | Open-Source @ TU/e (AMORE)"],
-            media: {
-                type: "video",
-                src: "videos/github_1.tiny.webm",
-                aspect: "landscape",
-            },
-            url: "https://github.com/openml-labs/gama/pull/212",
-        },
-        {
-            titles: ["GAMA x ConfigSpace"],
-            details: ["In-Review | Open-Source @ TU/e (AMORE)"],
-            media: {
-                type: "video",
-                src: "videos/github_2.tiny.webm",
-                aspect: "landscape",
-            },
-            url: "https://github.com/openml-labs/gama/pull/210",
         },
     ],
 };
